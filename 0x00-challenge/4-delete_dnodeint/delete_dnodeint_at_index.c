fc53f3b3a751fffa6389f329c1f88aaa8e0978db
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2023-05-17 20:52:52 -0700

    fixed

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
+		tmp = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
+			(*head)->next->prev = tmp;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
